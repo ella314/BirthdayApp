a4342b3ad2b333bed1ace77fb778c30421276d7d
Ella Ragoler <ella.ragoler@gmail.com>
2025-07-25 11:04:04 -0700

    Added birthdays at top and birthday calendar at bottom

--- BirthdayApp/ContentView.swift
+++ BirthdayApp/ContentView.swift
@@ -8,16 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var friends: [Friend] = [
+        Friend(name: "Dhriti", birthday: .now),
+        Friend(name: "Hana", birthday: Date(timeIntervalSince1970: 0))
+    ]
+    @State private var newName = ""
+    @State private var newBirthday = Date.now
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+        NavigationStack {
+            List(friends, id: \.name) { friend in
+                HStack {
+                    Text(friend.name)
+                    Spacer()
+                    Text(friend.birthday, format: .dateTime.month(.wide).day().year())
+                }
+            }
+            .navigationTitle("Birthdays")
+            .safeAreaInset(edge: .bottom) {
+                VStack (alignment : .center, spacing: 20) {
+                    Text("New Birthday")
+                        .font(.headline)
+                    DatePicker(selection: $newBirthday, in: Date.distantPast...Date.now, displayedComponents: .date) {
+                        TextField("Name", text: $newName)
+                            .textFieldStyle(.roundedBorder)
+                    }
+                    Button {
+                        
+                    }
+                    .padding()
+                } //Vstack
+            } //safeArea
+            
+        } //Navigation
+        
+    } //body
+} //struck
 
 #Preview {
     ContentView()
